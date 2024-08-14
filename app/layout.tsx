e6202dacb170557b51b8bb5da366bce0904b06f9
Alvin Vinod <alvincav@gmail.com>
2024-08-14 13:56:31 +0530

    added metadata and layout fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,27 @@ import {
 } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "TheKnowLedger",
+  description: "Organize your ideas and track metrics",
+  icons: { icon: "/assets/images/site-logo.svg" },
+};
 
 export default function RootLayout({
   children,
@@ -14,9 +35,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is some test</h1>
           <SignedOut>
             <SignInButton />
           </SignedOut>
