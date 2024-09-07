f61b855a8266514049cf1b8edbf476f7fa66ca92
Alvin Vinod <alvincav@gmail.com>
2024-09-08 01:03:56 +0530

    minor changes

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -6,22 +6,22 @@ export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
 
   if (!process.env.MONGODB_URL) {
-    return console.log("MONGODB_URL is not defined");
+    return console.log("MISSING MONGODB_URL");
   }
 
   if (isConnected) {
-    return console.log("MongoDB is already connected");
+    return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL!, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       dbName: "KnowLedger",
-      serverSelectionTimeoutMS: 60000, // Increase to 60 seconds
-      socketTimeoutMS: 45000,
     });
+
     isConnected = true;
+
     console.log("MongoDB is connected");
   } catch (error) {
-    console.log("Error connecting to MongoDB", error);
+    console.log("MongoDB connection failed", error);
   }
 };
