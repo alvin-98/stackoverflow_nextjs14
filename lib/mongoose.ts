a4015b5dadcac7cec3522edd39ceb28ebb111ed6
Alvin Vinod <alvincav@gmail.com>
2024-09-08 12:56:09 +0530

    fixed mongodb auth

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,7 +5,7 @@ let isConnected: boolean = false;
 export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGODB_URL) {
+  if (!process.env.MONGODB_URI) {
     return console.log("MISSING MONGODB_URL");
   }
 
@@ -14,7 +14,7 @@ export const connectToDatabase = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "KnowLedger",
     });
 
